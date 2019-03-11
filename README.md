a883475af4fdcd3a60289e8204594cdb3bfdff68
xxxDupaxxx <Kamil.orlowski@gmail.com>
2019-03-11 20:26:45 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-8-introduction
+
+1. overview - What's New in Java 8 - Jose Paumard
---
+++ overview/src/test/groovy/com/orlowski/AppTest.groovy
@@ -0,0 +1,16 @@
+package com.orlowski
+
+import spock.lang.Specification
+
+class AppTest extends Specification {
+    def "length of Spock's and his friends' names"() {
+        expect:
+        name.size() == length
+
+        where:
+        name     | length
+        "Spock"  | 5
+        "Kirk"   | 4
+        "Scotty" | 6
+    }
+}
