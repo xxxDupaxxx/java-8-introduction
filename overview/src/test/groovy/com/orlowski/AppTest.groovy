459f323fcf66691802b8d80c8b89588e05bc0914
xxxDupaxxx <Kamil.orlowski@gmail.com>
2019-04-29 19:32:12 +0200

    Added tests with Java 8 features

--- overview/src/test/groovy/com/orlowski/AppTest.groovy
+++ overview/src/test/groovy/com/orlowski/AppTest.groovy
@@ -2,15 +2,49 @@ package com.orlowski
 
 import spock.lang.Specification
 
-class AppTest extends Specification {
-    def "length of Spock's and his friends' names"() {
-        expect:
-        name.size() == length
+import java.util.function.Predicate
+import java.util.stream.Stream
 
-        where:
-        name     | length
-        "Spock"  | 5
-        "Kirk"   | 4
-        "Scotty" | 6
-    }
+class AppTest extends Specification {
+
+	def "Everything works when an interface declares abstract equals method - an interface is still a functional"() {
+		given:
+		MyInterface myImplTest = new MyImpl()
+
+		when:
+		def result = myImplTest.doSomething()
+
+		then:
+		result == "something"
+	}
+
+	def "filter data using static isEquals predicate method"() {
+		expect:
+		testedStream.filter(filter).count() == collectionSize
+
+		where:
+		testedStream             | filter                                            | collectionSize
+		Stream.of("a", "b", "c") | Predicate.isEqual("c")                            | 1
+		Stream.of("a", "b", "c") | Predicate.isEqual("c").or(Predicate.isEqual("b")) | 2
+	}
+
+	def "That's how to use a flatMap!"() {
+		given:
+		Map<String, List<List<String>>> listMap = new LinkedHashMap<>()
+		listMap.put("A", [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]])
+		listMap.put("B", [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]])
+		listMap.put("C", [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]])
+
+		when:
+		def result = listMap.entrySet()
+				.stream()
+				.map({ entrySet -> entrySet.getValue() }) //List<List<String>>>
+				.flatMap({ listOfStrings -> listOfStrings.stream() }) //Stream<List<String>>
+				.flatMap({ streamOfList -> streamOfList.stream() })
+				.map({ value -> value.toUpperCase() })
+				.count()
+
+		then:
+		result == 27
+	}
 }
